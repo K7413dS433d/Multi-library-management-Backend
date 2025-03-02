bd691e209874fe23dac6a285cc593fef98923128
K7413dS433d <khaledsaied2002@gmail.com>
2025-03-02 09:32:46 +0200

    Fix: pre user hook separate the phone encryption and pass hashing

--- src/database/models/User/user.hooks.js
+++ src/database/models/User/user.hooks.js
@@ -2,13 +2,14 @@ import { encrypt, hash } from "../../../utils/index.utils.js";
 
 // hash the password and encrypt phone
 export function preHashingData(next, doc) {
-  if (this.isModified("phone") || this.isModified("password")) {
+  if (this.isModified("phone"))
     this.phone = encrypt({ text: this.phone, secret: process.env.SECRET_KEY });
+
+  if (this.isModified("password"))
     this.password = hash({
       text: this.password,
       salt: +process.env.SALT_ROUND,
     });
-  }
 
   return next();
 }
